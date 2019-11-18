5694c9d290f27a1c3bd9dd420229a45e58c527c3
朱文赵 <1392936858@qq.com>
2019-11-18 21:28:59 +0800

    init

---
+++ src/main/java/com/ximo/springwebfluxinaction/SpringWebfluxInActionApplication.java
@@ -0,0 +1,13 @@
+package com.ximo.springwebfluxinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxInActionApplication.class, args);
+    }
+
+}
