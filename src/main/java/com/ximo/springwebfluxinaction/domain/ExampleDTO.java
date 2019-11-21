ae9dbf9ab97f16def1e9dbd7a5169bd11b6591e6
朱文赵 <1392936858@qq.com>
2019-11-22 00:14:33 +0800

    学些注解驱动的webflux

---
+++ src/main/java/com/ximo/springwebfluxinaction/domain/ExampleDTO.java
@@ -0,0 +1,20 @@
+package com.ximo.springwebfluxinaction.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author xikl
+ * @date 2019/11/21
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExampleDTO {
+
+    private Integer id;
+
+    private String name;
+
+}
